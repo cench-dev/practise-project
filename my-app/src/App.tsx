c916b8cfc24bc4146df27f3c69065714a480e47b
cench-dev <joystick1980@gmail.com>
2026-07-05 15:05:47 +0400

    fix: add styles for input hover

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,8 +1,17 @@
+import { useState } from 'react'
 import './App.css'
-
+import { Input } from './UI/Input/Input'
+import logInIcon from './assets/arrow-left-on-rectangle.svg';
 function App() {
+  const [text, setText] = useState("");
   return (
     <>
+      <Input
+        value={text}
+        placeholder='введите текст'
+        type='text'
+        onChange={(e) => setText(e.target.value)}
+        icon={logInIcon}/>
     </>
   )
 }
